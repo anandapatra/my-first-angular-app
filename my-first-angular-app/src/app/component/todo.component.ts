4da394be007fb9a4a389f19634b25c57708ec7f6
PATRA <ap2270@att.com>
2017-06-06 14:34:15 -0400

    Commiting Some Important Changes

--- my-first-angular-app/src/app/component/todo.component.ts
+++ my-first-angular-app/src/app/component/todo.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-todo',
@@ -8,17 +9,44 @@ export class TodoComponent {
 
     name : string;
     email : string;
-    phonenumber : string;
+    phonenumber : string;  
+    student: Student;
+    entries : Student[];
    
     constructor() {
         console.log('Student Component Intialized..')
-        this.name = 'Ananda';
+        this.name = 'Ananda Patra';
         this.email = 'patra.ananda@gmail.com';
-        this.phonenumber = '+1-721-666-7238';      
+        this.phonenumber = '+1-721-666-7238';
+        this.student = {
+            firstname: '',
+            emailId:'',
+            phoneNumber:''
+        }  
+        this.entries =[];        
     } 
+
+    saveData(form : NgForm) {
+        console.log('Calling Save Data' , form.value);
+        this.student.firstname = form.value.firstName;
+        this.student.emailId = form.value.emailId;
+        this.student.phoneNumber = form.value.phoneNumber;
+
+        this.entries.push(this.student);
+
+        console.log(this.entries);
+        
+    }    
+
+    deleteEntry(i: number) {
+         this.entries.splice(i);
+
+    }
 }
 
-interface address {
-    city : string;
-    state : string;
+export interface Student {
+    firstname : string;
+    emailId : string;
+    phoneNumber : string;
 }
+
